c3d8e581b6e9038b8e2965e57c2c718ff954d63a
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2023-11-02 13:13:11 -0700

    remove blog.js

--- public/blog.js
+++
@@ -1,9 +0,0 @@
-var iframe = document.querySelector('main #main-content content iframe')
-
-function check() {
-  if (iframe.window.location.href === 'https://www.blogger.com/profile/15375469230044994043') {
-    window.open('https://www.blogger.com/profile/15375469230044994043')
-    iframe.window.history.go(-1)
-  }
-}
-setInterval(1000, check)
