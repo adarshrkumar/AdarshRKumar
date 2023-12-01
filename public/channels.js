5606d2e5a6f95795511a48a944e8afe0772cf1eb
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2023-12-01 12:40:04 -0800

    update channels.js

--- public/channels.js
+++ public/channels.js
@@ -1,18 +1,50 @@
 var statsEle = document.querySelector('.channel-stats')
-var channel_ids = [
-    'UCb35v6VAVFI8Q0aIpwPoW1g', 
-    'UCLK0Lj3V864AbKfutfyztYA', 
-    'UC8cV2cdrp1ccBWzoi1R-UwQ', 
-    'UCpgY8iFp2Nkfu0jAwk2U94g'
+var props = [
+    {
+        name: 'id', 
+        default: '', 
+    }, 
+    {
+        name: 'layout', 
+        default: 'full', 
+    }, 
+    {
+        name: 'count', 
+        default: 'default', 
+    }, 
 ]
-channel_ids.forEach(function(cid, i) {
+var channels = [
+    {
+        id: 'UCb35v6VAVFI8Q0aIpwPoW1g', 
+    }, 
+    {
+        id: 'UCLK0Lj3V864AbKfutfyztYA', 
+    }, 
+    {
+        id: 'UC8cV2cdrp1ccBWzoi1R-UwQ', 
+    }, 
+    {
+        id: 'UCpgY8iFp2Nkfu0jAwk2U94g'
+    }
+]
+channels.forEach(function(c, i) {
     var container = document.createElement('div')
     container.classList.add('channel-container')
 
     statsEle.appendChild(container)
 })
 
-channel_ids.forEach(function(cid, i) {
+channels.forEach(function(c, i) {
+    layouts.forEach(function(l) {
+        if (!!l === false) c[l] = l.default
+    })
     var container = statsEle.querySelectorAll('.channel-container')[i]
-    container.innerHTML = `<div class="g-ytsubscribe" data-channelid="${cid}" data-layout="full" data-count="default"></div>`
+
+    var cEle = document.createElement('div')
+    cEle.classList.add('g-ytsubscribe')
+    cEle.setAttribute('data-channelid', c.id)
+    cEle.setAttribute('data-layout', c.layout)
+    cEle.setAttribute('data-count', c.count)
+
+    container.appendChild(cEle)
 })
