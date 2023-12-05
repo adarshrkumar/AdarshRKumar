0bbbdfd3daad70473957ee3f4bb3cb344c4881d5
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2023-12-05 12:37:12 -0800

    delete two files

--- public/eruda.js
+++ public/eruda.js
@@ -1,6 +1,6 @@
-var script = document.createElement('script')
-script.src="//cdn.jsdelivr.net/npm/eruda"
-document.body.appendChild(script)
+var script = document.createElement('script');
+script.src="//cdn.jsdelivr.net/npm/eruda";
+document.body.appendChild(script);
 script.onload = function () {
-    eruda.init()
-}
+    eruda.init();
+};
