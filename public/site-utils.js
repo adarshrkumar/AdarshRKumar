06312600d00c9271a8dd61f8f567049ae344877a
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-02-23 12:50:00 -0800

    update site-utils

--- public/site-utils.js
+++ public/site-utils.js
@@ -5,6 +5,8 @@ window.addEventListener('DOMContentLoaded', function(e) {
         if (lHref.startsWith('{baseDomain}')) {
             lHref = lHref.substring('{baseDomain}'.length)
             lHref = `https://adarshrkumar.dev/${lHref}`
+            if (lHref.includes('//')) lHref = lHref.replace('//', '/')
+            l.href = lHref
         }
     })
 })
