8101dd72b9771880b9afb4d42f703ed4b7717364
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-03-11 13:56:04 -0700

    update themeSwitcher.astro

---
+++ public/main.js
@@ -0,0 +1,37 @@
+var settingsKey = 'settings'
+
+function setSetting(name, value, type, key) {
+    var settings = localStorage.getItem(settingsKey)
+    settings = !!settings ? settings : '{}'
+    settings = JSON.parse(settings)
+
+    setting = settings[setting]
+    if (!!setting === false) {
+        switch(type) {
+            case 'object': 
+                setting = {}
+                break
+            case 'list': 
+                setting = []
+                break
+            default: 
+                setting = ''
+                break
+        }
+    }
+
+    switch(type) {
+        case 'object': 
+            setting[key] = value
+            break
+        case 'list': 
+            setting.push(value)
+            break
+        default: 
+            setting = value
+            break
+    }
+
+    settings = JSON.stringify(settings)
+    localStorage.setItem(settingsKey, settings)
+}
