f681264bb5e87b99e804d3d2cfe769e71c524ef7
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-05-20 12:49:28 -0700

    update autosize

--- public/scripts/autosize.js
+++ public/scripts/autosize.js
@@ -14,15 +14,3 @@ new MutationObserver(mutations => {
     });
   });
 }).observe(document.body, { childList: true });
-
-// Simulate injecting new <textarea> nodes
-setTimeout(() => {
-  const t1 = document.createElement("textarea");
-  t1.setAttribute("autosize", true);
-  t1.innerHTML = "Injected after page load. Will auto resize."
-  document.body.appendChild(t1);
-  
-  const t2 = document.createElement("textarea");
-  t2.innerHTML = "Injected after page load. Will NOT auto resize."
-  document.body.appendChild(t2);
-}, 1000)
