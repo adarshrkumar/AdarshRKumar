59b40586626861780e8eece04b567c542c815fcc
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-05-20 12:49:50 -0700

    update autosize.js

--- public/scripts/autosize.js
+++ public/scripts/autosize.js
@@ -1,8 +1,7 @@
 import autosize from 'https://cdn.skypack.dev/autosize@v4.0.2';
 
 // Autosize anything in the DOM on page load
-document.querySelectorAll("textarea[autosize]")
-  .forEach(autosize);
+document.querySelectorAll("textarea[autosize]").forEach(autosize);
 
 // Setup observer to autosize anything after page load
 new MutationObserver(mutations => {
