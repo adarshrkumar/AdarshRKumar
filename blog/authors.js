30242225642bd93cc67af8f47956c914027953c1
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-08-30 12:43:19 -0700

    update [id].astro

--- blog/authors.js
+++ blog/authors.js
@@ -1,6 +1,8 @@
-{
+var authors = {
     "adarshrkumar": {
         "displayName": "Adarsh R. Kumar", 
         "bio": "Hello, my name is Adarsh Kumar and i'm a High School Student. \nI enjoy coding, photography, music, astronomy history, and geography."
     }
 }
+
+export default authors
