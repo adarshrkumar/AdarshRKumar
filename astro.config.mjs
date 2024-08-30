e855fbf80f5d84f13ea74b9a70f79b7b10072262
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-08-30 11:27:34 -0700

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
     output: "server",
+    adapter: node({
+        mode: 'standalone',
+    }),
     redirects: {
         '/post/[...slug]': '/blogPost?id=[...slug]',
     }
