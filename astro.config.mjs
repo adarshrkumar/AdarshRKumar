cf190ce2ee429a99ec5b37ad07155af2d27b929d
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-09-04 01:38:57 +0000

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://adarshrkumar.dev',
     base: '/',
-    trailingSlash: 'ignore'
+    trailingSlash: 'ignore',
+    integrations: [sitemap()]
 });
