89042ec79d85353a2ddc11ec8d3c41ca3ee31233
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-09-03 18:36:11 -0700

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://adarshrkumar.dev',
-    base: '/'
+    base: '/',
+    trailingSlash: 'ignore'
 });
