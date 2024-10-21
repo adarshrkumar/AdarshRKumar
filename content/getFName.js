2756dacec0bc1e13b0e6e50e4b455ab7c7f9f89b
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-10-20 20:00:36 -0700

    create and reference getFName,js

---
+++ content/getFName.js
@@ -0,0 +1,13 @@
+function getFName(path) {
+    var repoName = 'AdarshRKumar.dev'
+
+    if (path.startsWith('/')) path - path.slice(1)
+    if (path.endsWith('/')) path = path.slice(0, -1)
+
+    if (path.includes('/')) path = path.split('/').slice(-1)[0]
+    if (path.endsWith('.md')) path = path.slice(0, -1*'.md'.length)
+
+    return path
+}
+
+export default getFName
