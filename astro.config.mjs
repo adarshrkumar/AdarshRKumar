54dcd81e833b1e4c948ca238143d575ed65b3fb7
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-11-12 12:27:13 -0800

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import path from 'path';
+import fs from 'fs';
+
+const directoryPath = path.join('./', 'content');
+const categories = fs.readdirSync(`${directoryPath}/photos`);
+
+const memberUrls = []
+categories.forEach((category, i) => {
+    var isDir = fs.lstatSync(`${directoryPath}/${file}`).isDirectory()
+    if (isDir) {
+        var files = fs.readdirSync(`${directoryPath}/${category}`)
+        files.forEach(file => {
+            try {
+                fs.copyFileSync(`${directoryPath}/${category}/${file}`, `${directoryPath}/allPhotos/${file}`, fs.constants.COPYFILE_EXCL)
+            }
+            catch (err) {  }
+        })
+    }
+});
 
 // https://astro.build/config
 export default defineConfig({
