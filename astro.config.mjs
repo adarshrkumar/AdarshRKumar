19a53af8b4f75a111e49e33d171c7f51737ae768
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-11-13 19:57:02 +0000

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,9 +13,16 @@ if (!fs.existsSync(`${directoryPath}/allPhotos`)) {
 categories.forEach((category, i) => {
     var isDir = fs.lstatSync(`${directoryPath}/photos/${category}`).isDirectory()
     if (isDir) {
-        var files = fs.readdirSync(`${directoryPath}/photos/${category}`)
-        files.forEach(file => {
-            fs.copyFileSync(`./${directoryPath}/photos/${category}/${file}`, `./${directoryPath}/allPhotos/${file}`, fs.constants.COPYFILE_EXCL)
+        var folders = fs.readdirSync(`${directoryPath}/photos/${category}`)
+        folders.forEach(folder => {
+            if (!fs.existsSync(`${directoryPath}/photos/${category}/${folder}`)) {
+                fs.mkdirSync(`${directoryPath}/photos/${category}/${folder}`);
+            }
+
+            var files = fs.readdirSync(`${directoryPath}/photos/${category}/${folder}`)
+            files.forEach(file => {
+                fs.copyFileSync(`./${directoryPath}/photos/${category}/${folder}/${file}`, `./${directoryPath}/allPhotos/${folder}/${file}`, fs.constants.COPYFILE_EXCL)
+            })
         })
     }
 });
