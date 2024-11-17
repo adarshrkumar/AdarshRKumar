760df5f65087963c08d279a4b9b2f68919b8bbe8
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-11-16 21:41:34 -0800

    update Script.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,25 @@ import fs from 'fs';
 const directoryPath = path.join('./', 'content');
 const categories = fs.readdirSync(`${directoryPath}/photos`);
 
+// if (fs.existsSync(`${directoryPath}/allPhotos`)) {
+//     const folders = fs.readdirSync(`${directoryPath}/allPhotos`); 
+//     if (folders) {
+//         if (folders.length > 0) {
+//             folders.forEach(folder => {
+//                 const files = fs.readdirSync(`${directoryPath}/allPhotos/${folder}`); 
+//                 if (files) {
+//                     if (files.length > 0) {
+//                         files.forEach(file => {
+//                             fs.unlinkSync(`${directoryPath}/allPhotos/${folder}/${file}`); 
+//                         })
+//                     }
+//                 }
+//                 fs.rmdirSync(`${directoryPath}/allPhotos/${folder}`); 
+//             })        
+//         }
+//     }
+//     fs.rmdirSync(`${directoryPath}/allPhotos`); 
+// }
 if (!fs.existsSync(`${directoryPath}/allPhotos`)) {
     fs.mkdirSync(`${directoryPath}/allPhotos`);
 }
@@ -21,7 +40,7 @@ categories.forEach((category, i) => {
 
             var files = fs.readdirSync(`${directoryPath}/photos/${category}/${folder}`)
             files.forEach(file => {
-                fs.copyFileSync(`./${directoryPath}/photos/${category}/${folder}/${file}`, `./${directoryPath}/allPhotos/${folder}/${file}`, fs.constants.COPYFILE_EXCL)
+                fs.copyFileSync(`./${directoryPath}/photos/${category}/${folder}/${file}`, `./${directoryPath}/allPhotos/${folder}/${file}`)
             })
         })
     }
