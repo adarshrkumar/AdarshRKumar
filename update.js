b3f745a08d3419be1ee13997b6854d06e330551e
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-11-20 17:36:08 +0000

    update astro.config.mjs

--- update.js
+++ update.js
@@ -10,26 +10,31 @@ function getFName(path) {
 const directoryPath = path.join('./', 'content');
 const categories = fs.readdirSync(`${directoryPath}/photos`);
 
-categories.forEach((category, i) => {
-    var isDir = fs.lstatSync(`${directoryPath}/photos/${category}`).isDirectory()
-    if (isDir) {
-        var files = fs.readdirSync(`${directoryPath}/photos/${category}`)
-        files.forEach(file => {
-            var isDir2 = fs.lstatSync(`${directoryPath}/photos/${category}/${file}`).isDirectory()
-            if (!isDir2) {
-                var obj = {
-                    name: getFName(file), 
-                    fullname: file, 
-                    extention: file.replaceAll(getFName(file), ''), 
-                    category: category, 
-                    uploader: 'adarshrkumar',
+update()
+function update() {
+    categories.forEach((category, i) => {
+        var isDir = fs.lstatSync(`${directoryPath}/photos/${category}`).isDirectory()
+        if (isDir) {
+            var files = fs.readdirSync(`${directoryPath}/photos/${category}`)
+            files.forEach(file => {
+                var isDir2 = fs.lstatSync(`${directoryPath}/photos/${category}/${file}`).isDirectory()
+                if (!isDir2) {
+                    var obj = {
+                        name: getFName(file), 
+                        fullname: file, 
+                        extention: file.replaceAll(getFName(file), ''), 
+                        category: category, 
+                        uploader: 'adarshrkumar',
+                    }
+                    if (!fs.existsSync(`./${directoryPath}/photos/${category}/${getFName(file)}`)) {
+                        fs.mkdirSync(`./${directoryPath}/photos/${category}/${getFName(file)}`);
+                    }
+                    fs.copyFileSync(`./${directoryPath}/photos/${category}/${file}`, `./${directoryPath}/photos/${category}/${getFName(file)}/${file}`)
+                    fs.writeFileSync(`./${directoryPath}/photos/${category}/${getFName(file)}/info.json`, JSON.stringify(obj, null, 4))
                 }
-                if (!fs.existsSync(`./${directoryPath}/photos/${category}/${getFName(file)}`)) {
-                    fs.mkdirSync(`./${directoryPath}/photos/${category}/${getFName(file)}`);
-                }
-                fs.copyFileSync(`./${directoryPath}/photos/${category}/${file}`, `./${directoryPath}/photos/${category}/${getFName(file)}/${file}`)
-                fs.writeFileSync(`./${directoryPath}/photos/${category}/${getFName(file)}/info.json`, JSON.stringify(obj, null, 4))
-            }
-        })
-    }
-});
+            })
+        }
+    });
+}
+
+export default update
