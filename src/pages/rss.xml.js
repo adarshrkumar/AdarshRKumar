53c4649adcacc99ed08dbe15c84914fed3728aaf
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2025-02-21 11:08:15 -0800

    Create rss.xml.js

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,35 @@
+import rss from '@astrojs/rss';
+
+const items = import.meta.glob('../../content/blog/posts/**/*.md', { eager: true })
+const keys = Object.keys(items)
+const posts = Object.values(items);
+
+import age from '../../content/getAge.js'
+
+const aboutFile = await import('../../content/aboutContent.md')
+var aboutContent = aboutFile.rawContent().replaceAll('{ age }', age.toString()).replaceAll('  ', ' ').replaceAll(' \n', ' ').replaceAll('\n', ' ')
+while (aboutContent.startsWith(' ')) aboutContent = aboutContent.slice(1)
+while (aboutContent.endsWith(' ')) aboutContent = aboutContent.slice(0, -1)
+
+
+export async function GET(context) {
+    return rss({
+        title: 'Buzz\'s Blog',
+        description: aboutContent,
+        site: context.site,
+        trailingSlash: context.trailingSlash,
+        items: posts.map((post, i) => {
+            var id = keys[i]
+            const sw = `../../content/blog/posts/`
+            if (id) if (id.startsWith(sw)) id = id.slice(sw.length)
+            return {
+                title: post.frontmatter.title,
+                pubDate: post.frontmatter.pubDate,
+                description: post.frontmatter.description,
+                // Compute RSS link from post `id`
+                // This example assumes all posts are rendered as `/blog/[id]` routes
+                link: `/post/${id}/`,
+            }
+        }),
+    });
+}
