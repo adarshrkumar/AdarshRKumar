b975602f8010591235d74afc9af221d4d8a8be97
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2025-02-21 11:07:22 -0800

    Create getAge.js

---
+++ content/getAge.js
@@ -0,0 +1,24 @@
+var dob = {
+    month: 3, 
+    day: 28, 
+    year: 2007, 
+}
+  
+var date = new Date()
+var currentDate = {
+    month: date.getMonth(), 
+    day: date.getDate(), 
+    year: date.getFullYear(), 
+}
+  
+let age = 0
+if (
+    (currentDate.month < dob.month) || 
+    (
+        currentDate.month === dob.month && 
+        currentDate.day <= 28
+    )
+) age = currentDate.year - dob.year - 1
+else age = currentDate.year - dob.year
+  
+export default age
