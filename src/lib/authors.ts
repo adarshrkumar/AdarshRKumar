0cda667add1da72424a1b85d483b4af5fd86f72e
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2025-10-20 11:26:11 -0700

    u

--- src/lib/authors.ts
+++ src/lib/authors.ts
@@ -1,11 +1,11 @@
 // Author data configuration
 const authors = {
-    "adarshrkumar": {
-        "displayName": "Adarsh R. Kumar", 
-        "bio": "Hello, my name is Adarsh Kumar and I'm a High School Student. \nI enjoy coding, photography, music, astronomy history, and geography.",
-        "hasPfp": true,
-        "pfp": {
-            "filename": "pfp.jpg"
+    'adarshrkumar': {
+        'displayName': 'Adarsh R. Kumar', 
+        'bio': "Hello, my name is Adarsh Kumar and I'm a High School Student. \nI enjoy coding, photography, music, astronomy history, and geography.",
+        'hasPfp': true,
+        'pfp': {
+            'filename': 'pfp.jpg'
         }
     }
 }
