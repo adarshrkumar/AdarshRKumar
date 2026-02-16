8a0e3e4e8e718a3626a6255170d58501781fc6a7
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2026-02-15 21:03:30 -0800

    u

---
+++ src/utils/categoryMap.ts
@@ -0,0 +1,8 @@
+// src/utils/categoryMap.ts
+export const categoryMap = {
+    'Design Softwares': { name: 'Design', icon: '🎨' },
+    'Other Tools': { name: 'Tools', icon: '🛠️' },
+    'Web Development - Front-End': { name: 'Front-End', icon: '🌐' },
+    'Web Development - Back End': { name: 'Back-End', icon: '⚙️' },
+    'Other Languages': { name: 'Languages', icon: '🗣️' }
+};
