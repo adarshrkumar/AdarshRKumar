9be98a8bcc3cbae9fc3c5230d45de4ebf5a67341
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2026-03-16 15:09:00 -0700

    u

--- tests/browser.test.nopause.ts
+++ tests/browser.test.nopause.ts
@@ -5,6 +5,5 @@ const base = 'http://localhost:4321';
 const path = process.env.BROWSER_TEST_PATH || '';
 
 test('test browser', async ({ page }) => {
-    // point this to wherever you want
     await page.goto(`${base}${path.startsWith('/') ? path : `/${path}`}`)
 })
--- tests/browser.test.ts
+++ tests/browser.test.ts
@@ -5,9 +5,6 @@ const base = 'http://localhost:4321';
 const path = process.env.BROWSER_TEST_PATH || '';
 
 test('test browser', async ({ page }) => {
-    // point this to wherever you want
     await page.goto(`${base}${path.startsWith('/') ? path : `/${path}`}`)
-
-    // keep browser open
     await page.pause()
 })
