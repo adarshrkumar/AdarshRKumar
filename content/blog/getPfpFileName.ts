b655dd913adee765799256aae3ee5b0b4c05b7e7
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2026-07-26 23:03:27 -0700

    u

--- content/blog/getPfpFileName.ts
+++ content/blog/getPfpFileName.ts
@@ -4,7 +4,7 @@ import path from 'path'
 /**
  * Gets profile picture filenames for a given username
  * Searches in the authors directory for files starting with 'pfp'
- * 
+ *
  * @param username - The username to search for profile pictures
  * @returns Array of profile picture filenames
  */
@@ -12,7 +12,7 @@ function getPfpFileName(username: string): string[] {
     try {
         const authorDirectory = path.join(__dirname, 'authors', username)
         const files = fs.readdirSync(authorDirectory)
-        
+
         // Filter for profile picture files (starting with 'pfp')
         return files.filter(file => file.startsWith('pfp'))
     } catch (error) {
