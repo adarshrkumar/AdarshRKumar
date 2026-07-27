4299b2e61aa4e57bc86921102ba5a1206bf9d645
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2026-07-27 09:26:49 -0700

    u

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 
+import parsers from './eslint/custom-parsers.js';
 import { rules, ruleConfigs } from './eslint/custom-rules.js';
 
 export default [
@@ -9,7 +10,7 @@ export default [
         ignores: ['node_modules', 'dist', '.astro', '.vercel']
     },
     {
-        files: ['**/*.{js,ts,mjs,cjs,tsx,jsx}'],
+        files: ['**/*.{js,ts,mjs,cjs,tsx,jsx,json}', 'eslint/**/*.js'],
         languageOptions: {
             parser: tsParser,
             ecmaVersion: 'latest',
@@ -49,6 +50,70 @@ export default [
                     maxEOF: 0
                 }
             ],
+            ...ruleConfigs,
+        }
+    },
+    {
+        files: ['src/**/*.astro'],
+        languageOptions: {
+            parser: parsers.astroParser,
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: {
+                console: 'readonly',
+                process: 'readonly',
+                Buffer: 'readonly',
+                fetch: 'readonly',
+                structuredClone: 'readonly',
+                Response: 'readonly',
+                URL: 'readonly',
+                File: 'readonly',
+                FormDataEntryValue: 'readonly',
+                Astro: 'readonly'
+            }
+        },
+        plugins: {
+            '@typescript-eslint': tsPlugin,
+            'custom': { rules },
+        },
+        rules: {
+            ...js.configs.recommended.rules,
+            ...tsPlugin.configs.recommended.rules,
+            '@typescript-eslint/no-explicit-any': 'error',
+            '@typescript-eslint/no-unused-vars': 'off',
+            'no-multiple-empty-lines': [
+                'error',
+                {
+                    max: 1,
+                    maxBOF: 0,
+                    maxEOF: 0
+                }
+            ],
+            ...ruleConfigs,
+        }
+    },
+    {
+        files: ['src/**/*.css'],
+        plugins: {
+            'custom': { rules },
+        },
+        rules: {
+            'arrow-parens': 'off',
+            ...ruleConfigs,
+        }
+    },
+    {
+        files: ['src/**/*.scss'],
+        languageOptions: {
+            parser: parsers.scssParser,
+            ecmaVersion: 'latest',
+            sourceType: 'module'
+        },
+        plugins: {
+            'custom': { rules },
+        },
+        rules: {
+            'arrow-parens': 'off',
             ...ruleConfigs,
         }
     },
---
+++ eslint/custom-parsers.js
@@ -0,0 +1,23 @@
+import fs from 'fs';
+
+const parserFiles = fs.readdirSync('./parsers').filter(f => f.endsWith('.js'));
+
+export default Object.fromEntries(
+    await Promise.all(
+        parserFiles.map(async file => {
+            const name = file.replace('.js', '').replace(/-([a-z])/g, (_, char) => char.toUpperCase());
+            const parseFunction = (await import(`./parsers/${file}`)).default;
+            const meta = {
+                name: name.replace(/([A-Z])/g, '-$1').toLowerCase(),
+                version: '1.0.0'
+            };
+            return [
+                name,
+                {
+                    parseForESLint: parseFunction,
+                    meta
+                }
+            ];
+        })
+    )
+);
